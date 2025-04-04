a7d1f63694c58d169ec74342328296b93a9985e2
Mohamad al halabi <info@tlkeys.com>
2025-04-04 16:13:43 +0300

    Latest Products has been added (api request added)

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,13 +1,22 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/product_model.dart';
+import '../../../../constants.dart';
 
 class ApiService {
-  static const String baseUrl = "https://your-api.com"; // Replace with actual API
 
   static Future<List<ProductModel>> fetchLatestProducts() async {
     try {
-      final response = await http.get(Uri.parse("$baseUrl/products/latest"));
+      final response = await http.get(Uri.parse("${AppConstants.baseUrl}/products/latest-products"),
+        headers: {
+          'Accept-Language': 'en',
+          'Content-Type': 'application/json',
+          'currency': 'USD',
+          'Accept': 'application/json',
+          'secret-key': AppConstants.secretKey,
+          'api-key': AppConstants.apiKey,
+        },
+      );
 
       if (response.statusCode == 200) {
         List<dynamic> jsonData = json.decode(response.body);
